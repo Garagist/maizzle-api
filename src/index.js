83a13cba6e0ac861727d732a27a5ae8cf6041627
David Spiola <david@spiola.com>
2021-06-01 16:03:08 +0200

    Update: change socket listen range

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Output from '@maizzle/framework/src/generators/output/index.js';
 import http from 'http'
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
